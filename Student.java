3fdc01e9e2450f73cfce6bfcc116dbfc0ccf1fc2
sleepyydesu <rachit2063@gmail.com>
2025-12-31 14:05:15 +0545

    Added getter methods to Student class

---
+++ Student.java
@@ -0,0 +1,19 @@
+package week7;
+
+public class Student {
+	private String name;
+	private int age;
+	
+	Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
